ea25413c06ff26c764c5d7fc5e232cfa67ada03a
diaa12360 <deyacc12@gmail.com>
2023-12-22 17:37:52 +0300

    User Done.

--- src/main/java/com/hashbus/back/database/data/access/TicketDAO.java
+++ src/main/java/com/hashbus/back/database/data/access/TicketDAO.java
@@ -4,6 +4,7 @@ import com.hashbus.back.database.mappers.PointMapper;
 import com.hashbus.back.database.mappers.TicketMapper;
 import com.hashbus.back.model.Ticket;
 import com.sun.source.tree.BreakTree;
+import jnr.ffi.annotations.In;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,33 +17,45 @@ import java.util.PrimitiveIterator;
 @AllArgsConstructor
 @Repository
 public class TicketDAO {
-    JdbcTemplate jdbcTemplate ;
+    JdbcTemplate jdbcTemplate;
     private TicketMapper ticketMapper;
 
-    //    public Ticket getTicketById(long ticket_id) {
-////        return jdbcTemplate.queryForObject("select * from ticket where ticket_id = ?  " , new Object [] {ticket_id} , new TicketMapper()) ;
-//    }
-//    public boolean insertTicket(){
-//
-//    }
     public List<Ticket> getTicketsByUserId(Integer id) {
         try {
-            return jdbcTemplate.query("select * from tickets where user_id = ?", new Object[]{id}, ticketMapper);
-        }catch (EmptyResultDataAccessException e){
+            return jdbcTemplate.query("select * from tickets where user_id = ? and used=0", new Object[]{id}, ticketMapper);
+        } catch (EmptyResultDataAccessException e) {
             return new ArrayList<>();
         }
     }
-    public Boolean buyTicket(Integer userId , Integer journeyId){
-        return jdbcTemplate.update("insert into tickets (user_id , journey_id) values (? , ? )" ,
-                userId ,
-                journeyId ) > 0 ;
+
+    public Boolean buyTicket(Integer userId, Integer journeyId) {
+        return jdbcTemplate.update("insert into tickets (user_id , journey_id, used) values (? , ?, 0)",
+                userId,
+                journeyId) > 0;
     }
+
     public Integer getNumberOfTickets() {
         return jdbcTemplate.queryForObject("select count(*) from tickets",
                 Integer.class);
     }
-    public List<Ticket> getAllTickets(){
-        return jdbcTemplate.query("select * from tickets" , ticketMapper) ;
+
+    public List<Ticket> getAllTickets() {
+        return jdbcTemplate.query("select * from tickets", ticketMapper);
+    }
+
+    public Boolean makeTheTicketUsedByUserIdAndJourneyId(Integer userId, Integer journeyId) {
+        boolean x =
+                jdbcTemplate.update("""
+                UPDATE tickets s
+                    JOIN (
+                        SELECT ticket_ID FROM tickets
+                        WHERE journey_ID = ? AND user_ID = ? AND used = 0
+                        LIMIT 1
+                    ) t ON s.ticket_ID = t.ticket_ID
+                SET s.used = 1 where s.used = 0;
+                """, journeyId, userId)
+                > 0;
+        return x;
     }
 
 }
